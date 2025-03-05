ff12251834a49d7e6bd87103fbafa68596b40e7f
Xinwei702 <xinweiw963@gmail.com>
2025-03-05 11:47:40 +0800

    week03

---
+++ week03-3.cpp
@@ -0,0 +1,12 @@
+///week03-3.cpp
+#include <stdio.h>
+int main()
+{
+    for(int i=0 ;i<5 ; i++){
+        printf("%d ",i);
+    }
+    printf("\n");
+    for(int i=1;i<=5;i++){
+        printf("%d ",i);
+    }
+}
---
+++ week03-4.cpp
@@ -0,0 +1,12 @@
+///week03-4.cpp M90H044
+#include <stdio.h>
+int main()
+{
+	int ans=0;
+	int n;
+	scanf("%d",&n);
+	for (int i=1;i<=n;i++){
+		ans=ans+i;	}
+	printf("%d",ans);
+
+}
