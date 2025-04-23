51031435359aaf88d2576719011e30a1b7323fc4
Xinwei702 <xinweiw963@gmail.com>
2025-04-23 12:02:39 +0800

    week10

---
+++ week10-2.cpp
@@ -0,0 +1,19 @@
+///week10-2.cpp
+#include <stdio.h>
+int main()
+{
+    printf("叫块计:");
+    int n;
+    scanf("%d",&n);
+    printf("块%d\n",n);
+    ///printf("计琌%d\n",n%10);
+    ///printf("计琌%d\n",n/10%10);
+    ///printf("κ计琌%d\n",n/10/10%10);
+    while(n>0){
+        printf("瞷р%dブ%d\n",n,n%10);
+        n=n/10;
+    }
+
+
+
+}
